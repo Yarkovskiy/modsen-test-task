c619e5d30d72377fe2d2e14d382ed1feddf66c47
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-08-30 19:32:50 +0300

    Add service classes

---
+++ src/main/java/com/example/modsentesttask/service/impl/GenreServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.modsentesttask.service.impl;
+
+import com.example.modsentesttask.domain.book.Genre;
+import com.example.modsentesttask.service.GenreService;
+
+public class GenreServiceImpl implements GenreService {
+
+    @Override
+    public Genre getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Genre getByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Genre update(Genre genre) {
+        return null;
+    }
+
+    @Override
+    public Genre create(Genre genre) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
---
+++ src/main/java/com/example/modsentesttask/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.modsentesttask.service.impl;
+
+import com.example.modsentesttask.domain.user.User;
+import com.example.modsentesttask.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User getByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public User update(User user) {
+        return null;
+    }
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
