3b5ad5d2715de7e5baf016972599d07935592ee4
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-09-01 18:16:30 +0300

    Add annotation

--- src/main/java/com/example/modsentesttask/web/mapper/BookLoanMapper.java
+++ src/main/java/com/example/modsentesttask/web/mapper/BookLoanMapper.java
@@ -3,7 +3,10 @@ package com.example.modsentesttask.web.mapper;
 import com.example.modsentesttask.domain.book.BookLoan;
 import com.example.modsentesttask.web.dto.request.BookLoanRequestDto;
 import com.example.modsentesttask.web.dto.response.BookLoanResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BookLoanMapper
         extends Mappable<BookLoan, BookLoanRequestDto, BookLoanResponseDto> {
 }
