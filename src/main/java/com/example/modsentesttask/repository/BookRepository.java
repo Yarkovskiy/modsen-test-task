361f3dcf16491c9e581032292c889181b7522f58
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-09-05 23:42:57 +0300

    Refactor BookRepository

--- src/main/java/com/example/modsentesttask/repository/BookRepository.java
+++ src/main/java/com/example/modsentesttask/repository/BookRepository.java
@@ -3,6 +3,15 @@ package com.example.modsentesttask.repository;
 import com.example.modsentesttask.domain.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+import java.util.Optional;
+
+public interface BookRepository
+        extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByIsbn(String isbn);
+
+    boolean existsByIsbn(String isbn);
+
+    boolean existsById(Long id);
 
 }
