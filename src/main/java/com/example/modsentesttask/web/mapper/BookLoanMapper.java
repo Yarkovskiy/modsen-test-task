2402320e65a26f9c6340fda8f1ec20ef41d94b50
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-09-05 23:43:30 +0300

    Refactor mappers

--- src/main/java/com/example/modsentesttask/web/mapper/BookLoanMapper.java
+++ src/main/java/com/example/modsentesttask/web/mapper/BookLoanMapper.java
@@ -1,12 +1,40 @@
 package com.example.modsentesttask.web.mapper;
 
+import com.example.modsentesttask.domain.book.Book;
 import com.example.modsentesttask.domain.book.BookLoan;
+import com.example.modsentesttask.domain.user.User;
+import com.example.modsentesttask.service.BookService;
+import com.example.modsentesttask.service.UserService;
 import com.example.modsentesttask.web.dto.request.BookLoanRequestDto;
 import com.example.modsentesttask.web.dto.response.BookLoanResponseDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
+import org.mapstruct.*;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BookLoanMapper
         extends Mappable<BookLoan, BookLoanRequestDto, BookLoanResponseDto> {
+
+    @Mapping(source = "bookId", target = "book", qualifiedByName = "mapBookIdToBook")
+    @Mapping(source = "userId", target = "user", qualifiedByName = "mapUserIdToUser")
+    BookLoan toEntity(
+            BookLoanRequestDto dto,
+            @Context BookService bookService,
+            @Context UserService userService
+    );
+
+    @Named("mapBookIdToBook")
+    default Book mapBookIdToBook(
+            Long bookId,
+            @Context BookService bookService
+    ) {
+        return bookService.getById(bookId);
+    }
+
+    @Named("mapUserIdToUser")
+    default User mapUserIdToUser(
+            Long userId,
+            @Context UserService userService
+    ) {
+        return userService.getById(userId);
+    }
+
 }
--- src/main/java/com/example/modsentesttask/web/mapper/BookMapper.java
+++ src/main/java/com/example/modsentesttask/web/mapper/BookMapper.java
@@ -1,13 +1,49 @@
 package com.example.modsentesttask.web.mapper;
 
+import com.example.modsentesttask.domain.book.Author;
 import com.example.modsentesttask.domain.book.Book;
+import com.example.modsentesttask.domain.book.Genre;
+import com.example.modsentesttask.service.AuthorService;
+import com.example.modsentesttask.service.GenreService;
 import com.example.modsentesttask.web.dto.request.BookRequestDto;
 import com.example.modsentesttask.web.dto.response.BookResponseDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
+import org.mapstruct.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BookMapper
         extends Mappable<Book, BookRequestDto, BookResponseDto> {
 
+    @Mapping(source = "genreIds", target = "genres", qualifiedByName = "mapGenreIdsToGenres")
+    @Mapping(source = "authorIds", target = "authors", qualifiedByName = "mapAuthorIdsToAuthors")
+    Book toEntity(
+            BookRequestDto dto,
+            @Context GenreService genreService,
+            @Context AuthorService authorService
+    );
+
+    @Named("mapGenreIdsToGenres")
+    default List<Genre> mapGenreIdsToGenres(
+            List<Long> genreIds,
+            @Context GenreService genreService
+    ) {
+        return genreIds.stream()
+                .map(genreService::getById)
+                .collect(Collectors.toList());
+    }
+
+    @Named("mapAuthorIdsToAuthors")
+    default List<Author> mapAuthorIdsToAuthors(
+            List<Long> authorIds,
+            @Context AuthorService authorService
+    ) {
+        return authorIds.stream()
+                .map(authorService::getById)
+                .collect(Collectors.toList());
+    }
+
 }
+
+
